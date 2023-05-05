39f4b63e44b7b2b46b605c2aafa74d44cc31c137
Wesley Klop <wesley19097@gmail.com>
2023-05-05 15:54:56 +0200

    Create form for making todos

--- todo-app/src/todo-app.ts
+++ todo-app/src/todo-app.ts
@@ -1,34 +1,66 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { map } from "lit/directives/map.js";
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+type Todo = {
+  content: string;
+  id: string;
+};
+
 @customElement("todo-app")
 export class TodoApp extends LitElement {
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0;
+  @property()
+  todos: Todo[] = [];
 
   render() {
     return html`
-      <button @click=${this._onClick} part="button">
-        count is ${this.count}
-      </button>
+      <ul>
+        ${map(this.todos, (todo: Todo) => html`<li>${todo.content}</li>`)}
+      </ul>
+      <form method="POST" @submit=${this.submitTodo}>
+        <input type="text" name="content" />
+        <button type="submit">maak todo</button>
+      </form>
     `;
   }
 
-  private _onClick() {
-    this.count++;
+  submitTodo(evt: SubmitEvent) {
+    evt.preventDefault();
+    const formData = new FormData(evt.currentTarget as HTMLFormElement);
+    const content = formData.get("content") || "";
+    if (typeof content !== "string") {
+      throw new Error("How is this not a string");
+    }
+    this.createTodo(content);
   }
 
   connectedCallback(): void {
     super.connectedCallback();
+    this.fetchTodos();
+  }
+
+  async fetchTodos() {
+    const response = await fetch("/api/todos", {
+      headers: {
+        accept: "application/json",
+      },
+    });
+    this.todos = await response.json();
+  }
+
+  async createTodo(content: string) {
+    const response = await fetch("/api/todos", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ content }),
+    });
+    if (response.status !== 201) {
+      console.error("Failed to create todo smh", response);
+    }
+    await this.fetchTodos();
   }
 
   static styles = css`
